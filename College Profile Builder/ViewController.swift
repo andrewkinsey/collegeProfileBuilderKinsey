bdbb1a15cbe28b97fac62e885df1864680ace88e
Andrew James Kinsey <akinsey@andrews-macbook-air-123.local>
2017-02-08 10:02:02 -0600

    Initial Commit

---
+++ College Profile Builder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  College Profile Builder
+//
+//  Created by Andrew James Kinsey on 2/8/17.
+//  Copyright © 2017 The Cabinents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
